6b38c5fd3293a5e5c64188016a19d827e980c960
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-03 16:40:41 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,12 +3,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: 3000,
-        dbURL: 'mongodb+srv://Nikeca1991:Zer0mancer@cluster0.f8lwx2u.mongodb.net/',
-        origin: ['http://localhost:5555', 'http://localhost:4200', "https://sozopolistudios.web.app"]
+        dbURL: 'mongodb+srv://Nikeca1991:Zer0Mancer321%21%40%21@cluster0.f8lwx2u.mongodb.net/',
+        origin: ['http://localhost:5555', 'http://localhost:4200', "https://sozopolistudios.web.app" , 'http://localhost:5173']
     },
     production: {
         port: 3000,
-        dbURL: 'mongodb+srv://Nikeca1991:Zer0mancer@cluster0.f8lwx2u.mongodb.net/',
+        dbURL: 'mongodb+srv://Nikeca1991:Zer0Mancer321%21%40%21@cluster0.f8lwx2u.mongodb.net/',
         origin: ["https://sozopolistudios.web.app",'http://localhost:4200', 'http://localhost:5173']
     }
 };
