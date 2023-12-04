dc6310e4f9114c57fd9892cf8b56f9f5d0b28ba9
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-04 02:35:56 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,12 +4,12 @@ const config = {
     development: {
         port: 3000,
         dbURL: 'mongodb+srv://nikeca1991:Zer0mancer@freephotos.clxlwry.mongodb.net/?retryWrites=true&w=majority',
-        origin: ['http://localhost:5555', 'http://localhost:4200', "https://sozopolistudios.web.app" , 'http://localhost:5173']
+        origin: ['http://localhost:5555', 'http://localhost:4200', "https://sozopolistudios.web.app" , 'http://localhost:5173', "https://free-photos-ac0ea.web.app"]
     },
     production: {
         port: 3000,
         dbURL: 'mongodb+srv://nikeca1991:Zer0mancer@freephotos.clxlwry.mongodb.net/?retryWrites=true&w=majority',
-        origin: ["https://sozopolistudios.web.app",'http://localhost:4200', 'http://localhost:5173']
+        origin: ["https://sozopolistudios.web.app",'http://localhost:4200', 'http://localhost:5173', "https://free-photos-ac0ea.web.app"]
     }
 };
 
